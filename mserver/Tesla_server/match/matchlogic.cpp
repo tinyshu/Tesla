578782f05b9b7d7f248f8a389f3cd2bf18b52ed5
loganwu <wuxiaolong63@gmail.com>
2016-12-10 19:00:16 +0800

    add match file

---
+++ mserver/Tesla_server/match/matchlogic.cpp
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  matchlogic.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12/10/16 18:59:29 CST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  loganwu (linux), wuxiaolong63@gmail.com
+ *        Company:  makedream
+ *
+ * =====================================================================================
+ */
+
+#include "matchlogic.h"
---
+++ mserver/Tesla_server/match/matchlogic.h
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ * 
+ *       Filename:  matchlogic.h
+ * 
+ *    Description:  
+ * 
+ *        Version:  1.0
+ *        Created:  12/10/16 18:59:15 CST
+ *       Revision:  none
+ *       Compiler:  gcc
+ * 
+ *         Author:  loganwu (linux), wuxiaolong63@gmail.com
+ *        Company:  makedream
+ * 
+ * =====================================================================================
+ */
+
+#ifndef  MATCHLOGIC_INC
+#define  MATCHLOGIC_INC
+
+#endif   /* ----- #ifndef MATCHLOGIC_INC  ----- */
+
